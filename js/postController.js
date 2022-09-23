32055e28845a4844c01074c3609f2b9745c7aca8
earthlydev <jarizala.dev@gmail.com>
2022-09-23 07:00:08 -0700

    updated app name, created new post controller

---
+++ js/postController.js
@@ -0,0 +1,17 @@
+class PostsController {
+    constructor(currentId = 0){
+        this.posts = [];
+        this.currentId = currentId;
+    }
+    addPost(title, caption, imgUrl, publicOrNot, location){
+        const newPost = {
+            id: this.currentId++,
+            title: title,
+            caption: caption,
+            imgUrl: imgUrl,
+            publicOrNot: publicOrNot,
+            location: location
+        }
+        this.posts.push(newPost);
+    }
+}
